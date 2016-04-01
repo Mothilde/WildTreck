a8a94eff2f9e95f0d95c2b114e1268c55c5f35fe
Mothilde <mathildenavet@hotmail.com>
2016-04-01 02:20:22 +0200

    il est 2h du mat

--- src/WildTreckBundle/Controller/DefaultController.php
+++ src/WildTreckBundle/Controller/DefaultController.php
@@ -10,4 +10,39 @@ class DefaultController extends Controller
     {
         return $this->render('WildTreckBundle:Default:index.html.twig');
     }
+
+    public function activiteAction()
+    {
+        return $this->render('WildTreckBundle:Default:activite.html.twig');
+    }
+
+    public function sportAction()
+    {
+        return $this->render('WildTreckBundle:Default:sport.html.twig');
+    }
+
+    /**
+     * Creates a new sport entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $nouveausport = new nouveausport();
+        $form = $this->createForm('WildTreckBundle\Form\nouveausportType', $nouveausport);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($nouveausport);
+            $em->flush();
+
+            return $this->redirectToRoute('nouveausport_show', array('id' => $nouveausport->getId()));
+        }
+
+        return $this->render('nouveausport/new.html.twig', array(
+            'nouveausport' => $nouveausport,
+            'form' => $form->createView(),
+        ));
+    }
 }
+
